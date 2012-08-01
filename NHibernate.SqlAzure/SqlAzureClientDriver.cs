2785de005c7f8107db04a9c248f64f7d134ebd19
Robert Moore <me@robdmoore.id.au>
2012-08-01 17:20:13 +0800

    Fixed Unity configuration issue

--- NHibernate.SqlAzure/SqlAzureClientDriver.cs
+++ NHibernate.SqlAzure/SqlAzureClientDriver.cs
@@ -1,7 +1,9 @@
-﻿using System.Data;
-using System.Data.SqlClient;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling;
 using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
-using NHibernate.AdoNet;
+using Microsoft.Practices.TransientFaultHandling;
 using NHibernate.Driver;
 
 namespace NHibernate.SqlAzure
@@ -10,7 +12,15 @@ namespace NHibernate.SqlAzure
     {
         public override IDbConnection CreateConnection()
         {
-            return new ReliableSqlConnection(null);
+            var retryStrategies = new List<RetryStrategy>();
+            const string incremental = "Incremental Retry Strategy";
+            const string interval = "Fixed Interval Retry Strategy";
+            const string backoff = "Backoff Retry Strategy";
+            retryStrategies.Add(new Incremental(incremental, 10, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1)));
+            retryStrategies.Add(new FixedInterval(interval, 10, TimeSpan.FromSeconds(1)));
+            retryStrategies.Add(new ExponentialBackoff(backoff, 10, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(30), TimeSpan.FromSeconds(10), false));
+            var retryManager = new RetryManagerImpl(retryStrategies, interval, backoff, incremental, interval, interval, interval);
+            return new ReliableSqlConnection(null, retryManager.GetDefaultSqlConnectionRetryPolicy(), retryManager.GetDefaultSqlCommandRetryPolicy());
         }
 
         /*public override IDbCommand CreateCommand()
