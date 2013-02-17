04751e073860cbba70659f3a9e149f6970b63dc3
Robert Moore <me@robdmoore.id.au>
2013-02-17 15:19:32 +0800

    Added event hooks to allow for separate logging of connection and command retries just in case anyone wants that

--- NHibernate.SqlAzure/SqlAzureClientDriver.cs
+++ NHibernate.SqlAzure/SqlAzureClientDriver.cs
@@ -24,13 +24,34 @@ namespace NHibernate.SqlAzure
             var retryManager = new RetryManagerImpl(retryStrategies, interval, backoff, incremental, interval, interval, interval);
 
             var connection = new ReliableSqlConnection(null, retryManager.GetDefaultSqlConnectionRetryPolicy(), retryManager.GetDefaultSqlCommandRetryPolicy());
-            connection.ConnectionRetryPolicy.Retrying += RetryEventHandler();
-            connection.CommandRetryPolicy.Retrying += RetryEventHandler();
+            connection.ConnectionRetryPolicy.Retrying += ConnectionRetryEventHandler();
+            connection.CommandRetryPolicy.Retrying += CommandRetryEventHandler();
             return connection;
         }
 
         /// <summary>
+        /// An event handler delegate which will be called on connection retries.
+        /// Only override this if you want to explicitly capture connection retries, otherwise override RetryEventHandler
+        /// </summary>
+        /// <returns>A custom method for handling the retry events</returns>
+        protected virtual EventHandler<RetryingEventArgs> ConnectionRetryEventHandler()
+        {
+            return RetryEventHandler();
+        }
+
+        /// <summary>
+        /// An event handler delegate which will be called on command retries.
+        /// Only override this if you want to explicitly capture command retries, otherwise override RetryEventHandler
+        /// </summary>
+        /// <returns>A custom method for handling the retry events</returns>
+        protected virtual EventHandler<RetryingEventArgs> CommandRetryEventHandler()
+        {
+            return RetryEventHandler();
+        }
+
+        /// <summary>
         /// An event handler delegate which will be called on connection and command retries.
+        /// If you override ConnectionRetryEventHandler and CommandRetryEventHandler then this is redundant.
         /// </summary>
         /// <returns>A custom method for handling the retry events</returns>
         protected virtual EventHandler<RetryingEventArgs> RetryEventHandler()
