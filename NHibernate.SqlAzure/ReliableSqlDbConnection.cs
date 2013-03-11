b79418eb89051e5face47c962bb183a6e76019f8
Robert Moore <me@robdmoore.id.au>
2013-03-11 20:10:42 +0800

    Adding retry code to the GetSchema calls to fix GH-7

--- NHibernate.SqlAzure/ReliableSqlDbConnection.cs
+++ NHibernate.SqlAzure/ReliableSqlDbConnection.cs
@@ -55,16 +55,25 @@ namespace NHibernate.SqlAzure
             ReliableConnection.Close();
         }
 
-        public override DataTable GetSchema() {
-            return ReliableConnection.Current.GetSchema();
+        public override DataTable GetSchema()
+        {
+            return ReliableConnection.ConnectionRetryPolicy.ExecuteAction (
+                () => ReliableConnection.Current.GetSchema()
+            );
         }
 
-        public override DataTable GetSchema(string collectionName) {
-            return ReliableConnection.Current.GetSchema(collectionName);
+        public override DataTable GetSchema(string collectionName)
+        {
+            return ReliableConnection.ConnectionRetryPolicy.ExecuteAction (
+                () => ReliableConnection.Current.GetSchema(collectionName)
+            );
         }
 
-        public override DataTable GetSchema(string collectionName, string[] restrictionValues) {
-            return ReliableConnection.Current.GetSchema(collectionName, restrictionValues);
+        public override DataTable GetSchema(string collectionName, string[] restrictionValues)
+        {
+            return ReliableConnection.ConnectionRetryPolicy.ExecuteAction (
+                () => ReliableConnection.Current.GetSchema(collectionName, restrictionValues)
+            );
         }
 
         public override void ChangeDatabase(string databaseName)
