e9b6a0d3a0eac0bc7bd0949010cebfb2b6fc74ca
Matt Davies <matt@mdavies.net>
2018-02-18 15:51:35 +1100

    Give further opportunity for non batching commands test on non-retrying driver to fail

--- NHibernate.SqlAzure.Tests/SqlClientDriverTests.cs
+++ NHibernate.SqlAzure.Tests/SqlClientDriverTests.cs
@@ -62,7 +62,7 @@ namespace NHibernate.SqlAzure.Tests
             {
                 using (TemporarilyShutdownSqlServerExpress())
                 {
-                    for (var i = 0; i < 500; i++)
+                    for (var i = 0; i < 1000; i++)
                     {
                         Insert_and_select_entity();
                         Thread.Sleep(50);
